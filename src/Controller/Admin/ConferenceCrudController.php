6c57e4a018bba6227b8476e3c79facc9992f62e2
luisriego <luisriego@hotmail.com>
2023-09-17 21:41:19 -0300

    Basic admin

---
+++ src/Controller/Admin/ConferenceCrudController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Conference;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
+
+class ConferenceCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Conference::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Conference')
+            ->setEntityLabelInPlural('Conferences')
+            ->setSearchFields(['city', 'year', 'isInternational'])
+            ->setDefaultSort(['year' => 'DESC'])
+            ;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        yield TextField::new('city');
+        yield TextField::new('year');
+        yield BooleanField::new('isInternational')
+        ;
+    }
+}
